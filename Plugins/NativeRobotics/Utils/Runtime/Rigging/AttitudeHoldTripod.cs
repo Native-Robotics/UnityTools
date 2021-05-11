742928d19c64fb073689d54c3bc688e4ba5a696b
nchernyadev <nikita.chernyadev@gmail.com>
2021-05-11 13:43:09 +0300

    RodBellow added, HoldTripod optimized

--- Plugins/NativeRobotics/Utils/Runtime/Rigging/AttitudeHoldTripod.cs
+++ Plugins/NativeRobotics/Utils/Runtime/Rigging/AttitudeHoldTripod.cs
@@ -5,7 +5,7 @@ namespace NativeRobotics.Utils.Rigging
 {
     public class AttitudeHoldTripod : MonoBehaviour
     {
-        [Header("Reference settings")] 
+        [Header("Reference settings")]
         public Transform reference;
 
         [Header("Tripod settings")]
@@ -13,10 +13,17 @@ namespace NativeRobotics.Utils.Rigging
 
         public float offset;
 
+        private Quaternion prevReferenceLocalRotation = Quaternion.identity;
+
         private void Update()
         {
-            reference.localRotation.ToAngleAxis(out var angle, out var axis);
-            transform.localRotation = Quaternion.AngleAxis(sign * (angle + offset), axis);
+            var referenceLocalRotation = reference.localRotation;
+            if (prevReferenceLocalRotation == referenceLocalRotation) return;
+
+            referenceLocalRotation.ToAngleAxis(out var referenceAngle, out var referenceAxis);
+            transform.localRotation = Quaternion.AngleAxis(sign * (referenceAngle + offset), referenceAxis);
+
+            prevReferenceLocalRotation = referenceLocalRotation;
         }
     }
 }
---
+++ Plugins/NativeRobotics/Utils/Runtime/Rigging/RodBellow.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace NativeRobotics.Utils.Rigging
+{
+    public class RodBellow : MonoBehaviour
+    {
+        [Header("Transforms")]
+        public Transform a;
+        public Transform b;
+        
+        [Header("Distances")]
+        public float distanceConstant = 0.1036497f;
+        public Vector3 stretchAxis = Vector3.one;
+
+        private float initialDistance;
+        private float currentDistance;
+
+        private Vector3 prevPosA = Vector3.zero;
+        private Vector3 prevPosB = Vector3.zero;
+        private Vector3 constantScale = Vector3.one;
+
+        private void Awake()
+        {
+            initialDistance = Vector3.Distance(a.position, b.position) - distanceConstant;
+            constantScale = Vector3.Scale(Vector3.one, Vector3.one - stretchAxis);
+        }
+
+        private void Update()
+        {
+            if (a.position == prevPosA && b.position == prevPosB) return;
+
+            var positionA = a.position;
+            var positionB = b.position;
+
+            currentDistance = Vector3.Distance(positionA, positionB);
+            var scaleFactor = (currentDistance - distanceConstant) / initialDistance;
+
+            transform.localScale = constantScale + stretchAxis * scaleFactor;
+            
+            prevPosA = positionA;
+            prevPosB = positionB;
+        }
+    }
+}
