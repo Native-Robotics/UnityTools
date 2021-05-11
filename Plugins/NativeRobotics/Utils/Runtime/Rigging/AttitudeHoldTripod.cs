af6434765834adb7b9533f89526f0a4415114da4
nchernyadev <nikita.chernyadev@gmail.com>
2021-05-11 13:06:40 +0300

    Rigging utils added to package

---
+++ Plugins/NativeRobotics/Utils/Runtime/Rigging/AttitudeHoldTripod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace NativeRobotics.Utils.Rigging
+{
+    public class AttitudeHoldTripod : MonoBehaviour
+    {
+        [Header("Reference settings")] 
+        public Transform reference;
+
+        [Header("Tripod settings")]
+        public int sign;
+
+        public float offset;
+
+        private void Update()
+        {
+            reference.localRotation.ToAngleAxis(out var angle, out var axis);
+            transform.localRotation = Quaternion.AngleAxis(sign * (angle + offset), axis);
+        }
+    }
+}
