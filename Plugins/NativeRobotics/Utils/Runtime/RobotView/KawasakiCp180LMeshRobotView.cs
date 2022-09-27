244ea9f643316b67fa0b62ca507e10d0031ed54e
asquad <awesomequality@gmail.com>
2022-09-27 18:26:38 +0300

    feature: temporary dummy visualizer for Kawasaki cp180l

---
+++ Plugins/NativeRobotics/Utils/Runtime/RobotView/KawasakiCp180LMeshRobotView.cs
@@ -0,0 +1,11 @@
+﻿namespace NativeRobotics.Utils.RobotView
+{
+    public class KawasakiCp180LMeshRobotView : MeshRobotView
+    {
+        public override void UpdateView(float[] state)
+        {
+            state[2] = state[2] - state[1];
+            base.UpdateView(state);
+        }
+    }
+}
